5a4a698e708bd73cbbb48b0128b4f99c383244f9
Lisyk <sergey.liskovoy@gmail.com>
2015-07-03 11:55:36 -0400

    add and customize devise plus time zones

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem "font-awesome-rails"
 
 gem 'simple_form'
 
+gem 'devise'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def user_time_zome(&block)
+    Time.use_zone(current_user.time_zone, &block)
+  end
 end
