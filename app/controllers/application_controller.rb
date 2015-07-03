66f34317059bcb69f224a4d08c4e6cb00d51faab
Lisyk <sergey.liskovoy@gmail.com>
2015-07-03 12:21:16 -0400

    update header for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  around_filter :user_time_zome, if: :current_user
+
   private
 
   def user_time_zome(&block)
